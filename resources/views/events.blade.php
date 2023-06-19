9aa4312171083ba4b0d15d651900cbaaed743aa4
Percian Joseph Borja <percian0296@gmail.com>
2023-06-19 11:57:06 +0800

    PROJECT 1

---
+++ resources/views/events.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+
+@section('content')
+    <h1>Welcome to Events Page</h1>
+@endsection
+
+@section('title')
+    Events Page
+@endsection
---
+++ resources/views/news.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+<h1>You are on the News Page</h1>
+@endsection
+
+@section('title')
+    News Page
+@endsection
